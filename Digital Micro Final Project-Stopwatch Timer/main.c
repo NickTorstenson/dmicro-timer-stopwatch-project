de4c52392f83302c85c5dc456e4521829ba291be
Torstenson <20603772+NickTorstenson@users.noreply.github.com>
2026-04-07 11:20:24 -0500

    Initial Commit

---
+++ Digital Micro Final Project-Stopwatch Timer/main.c
@@ -0,0 +1,18 @@
+/*
+ * Digital Micro Final Project-Stopwatch Timer.c
+ *
+ * Created: 4/7/2026 11:14:16 AM
+ * Author : tornich
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
