8359bc58e6f42eb8ce8205f166ae0a1ed30c8871
Torstenson <20603772+NickTorstenson@users.noreply.github.com>
2026-04-21 15:00:17 -0500

    Clean repo with updated gitignore

--- Digital Micro Final Project-Stopwatch Timer/Debug/stopwatch_timer_tools.d
+++
@@ -1,7 +0,0 @@
-stopwatch_timer_tools.d stopwatch_timer_tools.o: \
- .././stopwatch_timer_tools.c .././stopwatch_timer_tools.h \
- .././digital_io.h
-
-.././stopwatch_timer_tools.h:
-
-.././digital_io.h:
